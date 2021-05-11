f9f4ce2bb82351ab29abc19357d21c98e9856a73
healthpackdev <trhealthpack@gmail.com>
2021-05-11 18:15:31 +0300

    :crown: add application scripts sitemap and add icons

---
+++ src/scripts/add-icons.ts
@@ -0,0 +1,27 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faDiscord, faTwitter, faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import {
+  faAngleDoubleRight,
+  faArrowUp,
+  faMoon,
+  faSun,
+  faStar,
+  faCodeBranch,
+  faBars,
+} from '@fortawesome/free-solid-svg-icons';
+
+const icons = [
+  faDiscord,
+  faTwitter,
+  faGithub,
+  faYoutube,
+  faAngleDoubleRight,
+  faArrowUp,
+  faMoon,
+  faSun,
+  faStar,
+  faBars,
+  faCodeBranch,
+];
+
+library.add(...icons);
---
+++ src/scripts/sitemap.js
@@ -0,0 +1,3 @@
+// eslint-disable-next-line no-console
+
+console.log('Sitemap generated.');
