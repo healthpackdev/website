1543854280447ad54dd2ade37fdbcd4b747e68f0
healthpackdev <trhealthpack@gmail.com>
2021-05-18 22:25:37 +0300

    fix: remove any spread, import type from use motion

--- src/components/containers/index.tsx
+++ src/components/containers/index.tsx
@@ -1,13 +1,13 @@
-/* eslint-disable import/prefer-default-export */
-
-import { ContainerProps, Container as ChakraContainer, Text } from '@chakra-ui/react';
+import { ContainerProps, Container as ChakraContainer, Text, GridItem } from '@chakra-ui/react';
+import useMotion, { MotionElementProps } from '@hooks/use-motion';
+import { VariantLabels, TargetAndTransition } from 'framer-motion';
 
 interface ContainerExtendedProps extends ContainerProps {
   header: string;
 }
 const Container: React.FC<ContainerExtendedProps> = ({ header, children, ...props }) => (
-  <ChakraContainer maxW="container.lg" py="6" as="section" {...props}>
-    <Text as="header" fontSize="3xl" textAlign="center" py="2" fontWeight="semibold">
+  <ChakraContainer maxW="container.lg" py="10" as="section" {...props}>
+    <Text as="header" fontSize="3xl" textAlign="center" my="7" fontWeight="semibold">
       {header}
     </Text>
     {children}
@@ -15,3 +15,14 @@ const Container: React.FC<ContainerExtendedProps> = ({ header, children, ...prop
 );
 
 export { Container };
+
+interface CardProps extends MotionElementProps {
+  hover?: VariantLabels | TargetAndTransition;
+}
+const MotionGridItem = useMotion(GridItem);
+const Card: React.FC<CardProps> = ({ children, hover, ...props }) => (
+  <MotionGridItem whileHover={hover} className="card" cursor="pointer" boxShadow="md" borderRadius="md" {...props}>
+    {children}
+  </MotionGridItem>
+);
+export { Card };
