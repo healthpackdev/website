95a10723844ea61192856e5e5861cca0f71ffba9
healthpackdev <trhealthpack@gmail.com>
2021-05-18 20:24:23 +0300

    improve typings

--- src/hooks/use-motion.tsx
+++ src/hooks/use-motion.tsx
@@ -1,11 +1,11 @@
 import { ComponentWithAs, HTMLChakraProps, As } from '@chakra-ui/react';
 import { motion, HTMLMotionProps } from 'framer-motion';
 
-// * this is not a hook
+type Merge<P, T> = Omit<P, keyof T> & T;
+export type MotionElementProps = Merge<HTMLChakraProps<As>, HTMLMotionProps<any>>;
+
 function useMotion(component: ComponentWithAs<any, any>) {
-  type Merge<P, T> = Omit<P, keyof T> & T;
-  type MotionElementprops = Merge<HTMLChakraProps<As<any>>, HTMLMotionProps<any>>;
-  const MotionElement: React.FC<MotionElementprops> = motion(component);
+  const MotionElement: React.FC<MotionElementProps> = motion(component);
   return MotionElement;
 }
 export default useMotion;
