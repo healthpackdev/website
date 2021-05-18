da2c83415ecebf2d55ac367f4c1f9ae745e807fc
healthpackdev <trhealthpack@gmail.com>
2021-05-18 22:30:51 +0300

    Improve seo in layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,12 +1,19 @@
-import { Portal, Box } from '@chakra-ui/react';
+import { Portal, Box, BoxProps } from '@chakra-ui/react';
 import Navbar from '@components/layout/Navbar';
 import Footer from '@components/layout/Footer';
 import ScrollTop from '@components/common/scroll-top';
+import { Seo } from '@config/seo';
+import type { DefaultSeoProps } from 'next-seo';
 
-const Layout: React.FC = ({ children }) => (
+interface LayoutProps extends BoxProps {
+  seo: DefaultSeoProps;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children, seo, ...props }) => (
   <>
+    <Seo {...seo} />
     <Navbar />
-    <Box as="main" minH="100%">
+    <Box as="main" minH="100%" {...props}>
       {children}
     </Box>
     <Portal>
