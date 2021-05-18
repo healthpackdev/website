30caedfe1658761e337040fce89277f40d7b736a
healthpackdev <trhealthpack@gmail.com>
2021-05-18 20:23:10 +0300

    rewrite _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,10 @@ import { AppProps } from 'next/app';
 import { ThemeProvider } from '@theme/index';
 import '@scripts/add-icons';
 
-function App({ Component, pageProps }: AppProps) {
-
-  return (
-    <ThemeProvider cookies={pageProps.cookies}>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <ThemeProvider cookies={pageProps.cookies}>
+    <Component {...pageProps} />
+  </ThemeProvider>
+);
 
 export default App;
