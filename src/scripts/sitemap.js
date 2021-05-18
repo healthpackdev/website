4cea60a2977e6713dd1511254e69b8e20e307cf4
healthpackdev <trhealthpack@gmail.com>
2021-05-18 20:16:37 +0300

    write sitemap script for seo

--- src/scripts/sitemap.js
+++ src/scripts/sitemap.js
@@ -1,3 +1,29 @@
-// eslint-disable-next-line no-console
+const globby = require('globby');
+const fs = require('fs');
+const prettier = require('prettier');
+const siteConfig = require('../../config/site-config.json');
 
-console.log('Sitemap generated.');
+globby(['src/pages/*', '!src/pages/_*', '!src/pages/api', '!src/pages/404.tsx']).then((pages) => {
+  const sitemapTemplate = [
+    `<?xml version="1.0" encoding="UTF-8"?>`,
+    `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`,
+    ...pages.map((page) => {
+      const path = page.replace(/(src\/|pages|\.[a-z]+|\/index)/g, '');
+
+      return `
+      <url>
+      <loc>https://${siteConfig.host_name + path}</loc>
+      </url>
+      `;
+    }),
+    `</urlset>`,
+  ];
+  const sitemap = prettier.format(sitemapTemplate.join(''), {
+    parser: 'html',
+  });
+
+  fs.writeFile('public/sitemap.xml', sitemap, { encoding: 'utf-8' }, (err) => {
+    if (err) throw new Error(err);
+    console.log('Sitemap generated.');
+  });
+});
