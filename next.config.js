edb7ed520e4e2e392f9a33c5cea25533d8bb1776
healthpackdev <trhealthpack@gmail.com>
2021-05-19 15:40:47 +0300

    remove: next-compose-plugins

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
-const withPlugins = require('next-compose-plugins');
-
-module.exports = withPlugins([
-  {
-    future: { webpack5: true },
-    jsconfigPaths: true,
-    distDir: 'build',
-    webpack(config, { isServer, dev }) {
-      if (isServer && process.env.NODE_ENV === "production") {
-        require('./src/scripts/sitemap.js');
-        // require('./src/scripts/favicons.js'); depreceted
-      }
-      return config;
-    },
+module.exports = {
+  future: { webpack5: true },
+  jsconfigPaths: true,
+  distDir: 'build',
+  webpack(config, { isServer, dev }) {
+    if (isServer && process.env.NODE_ENV === 'production') {
+      require('./src/scripts/sitemap.js');
+      // require('./src/scripts/favicons.js'); depreceted
+    }
+    return config;
   },
-]);
+};
