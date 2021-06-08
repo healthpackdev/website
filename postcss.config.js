2169adc2be9d7f8efcb9954e21bbbd28c6f7f94b
healthpackdev <yasinkadirkilic@gmail.com>
2021-06-08 20:52:52 +0300

    build: add postcss-combine-media-query

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
 const config = {
-  plugins: ['tailwindcss',  'postcss-combine-duplicated-selectors'],
+  plugins: ['tailwindcss', 'postcss-combine-duplicated-selectors', 'postcss-combine-media-query'],
 };
 if (process.env.NODE_ENV === 'production') config.plugins.push('autoprefixer');
 
