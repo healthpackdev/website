ed7ceb4db6ac64fdb36233e892bb8a27f510a2ab
healthpackdev <yasinkadirkilic@gmail.com>
2021-06-08 20:53:46 +0300

    build: add preact/compat, bundle analyzer, remove webpack 5 redirect socials

--- next.config.js
+++ next.config.js
@@ -1,12 +1,32 @@
-module.exports = {
-  future: { webpack5: true },
+const author = require('./config/author-meta.json');
+const withAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withAnalyzer({
   jsconfigPaths: true,
   distDir: 'build',
   webpack(config, { isServer, dev }) {
-    if (isServer && process.env.NODE_ENV === 'production') {
+    if (!isServer && !dev) {
       require('./src/scripts/sitemap.js');
-      // require('./src/scripts/favicons.js'); depreceted
+
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
     }
     return config;
   },
-};
+  async redirects() {
+    return author.socials.map((social) => {
+      const source = `/${social.icon[1]}`; // ['fab','discord']
+      const redirect = social.href;
+      return {
+        source: source,
+        destination: redirect,
+        permanent: true,
+      };
+    });
+  },
+});
