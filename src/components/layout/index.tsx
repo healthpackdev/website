c9a101dbb160f9e17c6f67b4590866861071d105
healthpackdev <yasinkadirkilic@gmail.com>
2021-06-13 17:57:34 +0300

    chore: move scroll-top component into layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,9 +1,9 @@
 import Navbar from '@components/layout/Navbar';
 import Footer from '@components/layout/Footer';
-import ScrollTop from '@components/common/scroll-top';
+import ScrollTop from '@components/layout/scroll-top';
+import Seo from '@components/layout/Seo';
 import { DefaultSeoProps } from 'next-seo';
 import { HTMLMotionProps, motion } from 'framer-motion';
-import Seo from '@components/layout/Seo';
 
 interface LayoutProps {
   seo: DefaultSeoProps;
@@ -14,6 +14,7 @@ const Layout: React.FC<LayoutProps & HTMLMotionProps<'main'>> = ({ children, seo
     <Seo {...seo} />
     <Navbar />
     <motion.main
+      className="content"
       initial={{ y: 5, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.4 }}
