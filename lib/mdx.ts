80f9242ee572128ed324b46a511adc7e73323ffd
healthpackdev <yasinkadirkilic@gmail.com>
2021-06-13 17:58:24 +0300

    chore: add lib folder into root

---
+++ lib/mdx.ts
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const processRoot = process.cwd();
+
+export interface IBlogPost {
+  data: {
+    image: string;
+    title: string;
+    description: string;
+    publishedAt: Date;
+    categories: string[];
+  };
+  content: string;
+  slug: string;
+}
+
+export const getBlogPosts = (): IBlogPost[] => {
+  const fileNames = fs.readdirSync(path.join(processRoot, 'content', 'blog'));
+  return fileNames.reduce((allPosts, currentPost) => {
+    const source = fs.readFileSync(path.join(processRoot, 'content', 'blog', currentPost), 'utf-8');
+    const { data, content } = matter(source); // content for readmore
+    return [{ data, content, slug: currentPost.replace('.mdx', '') }, ...allPosts];
+  }, []);
+};
