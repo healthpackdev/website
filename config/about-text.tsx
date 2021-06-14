1920b20086ef955c881dcafa63695e034c3415ff
healthpackdev <yasinkadirkilic@gmail.com>
2021-06-14 17:06:22 +0300

    style: change About text

--- config/about-text.tsx
+++ config/about-text.tsx
@@ -1,7 +1,10 @@
-const AboutText = () => (
+import Link from 'next/link';
+
+const AboutText: React.FC = () => (
   <>
     Hi my name is <strong className="text-blue-500 dark:text-blue-400">Yasin Kadir</strong>. I am young full-stack web
-    developer from turkey 🇹🇷. I recently started build simple websites and you are here in my website!
+    developer from turkey 🇹🇷. I recently started build simple websites and you are here in my website! I love code
+    Javascript and Typescript for more information scroll down!
   </>
 );
 
