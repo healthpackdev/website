974b18edd78867a3d430e2d33337c060b08b3fc4
healthpackdev <yasinkadirkilic@gmail.com>
2021-06-15 19:58:14 +0300

    ui: remove avatar

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,18 +1,7 @@
 import HeroAbout from '@components/Hero/hero-about';
-import { motion } from 'framer-motion';
-// import Image from 'next/image';
 
-// const MotionImg = motion(Image); // don't use that
 const Hero: React.FC = () => (
-  <section id="hero" className="container flex items-center lg:flex-row flex-col lg:py-20 py-16">
-    <motion.img
-      src="/avatar.png"
-      alt="Profile photo"
-      width={125}
-      height={125}
-      className="mx-5 cursor-pointer shadow-lg rounded-full lg:order-1"
-      whileHover={{ translateY: '-10px' }}
-    />
+  <section id="hero" className="flex items-center lg:my-20 my-16">
     <HeroAbout />
   </section>
 );
