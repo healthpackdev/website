d8afd14dc9bdbea2cc4c92f16011eb9718826092
healthpackdev <yasinkadirkilic@gmail.com>
2021-06-15 19:53:09 +0300

    ui: text-white in scroll top icon

--- src/components/Layout/scroll-top.tsx
+++ src/components/Layout/scroll-top.tsx
@@ -19,7 +19,7 @@ const ScrollTop: React.FC = () => {
     !isTop && (
       <button
         type="button"
-        className="fixed bottom-5 right-5 w-10 h-10 rounded-full dark:bg-blue-500 bg-blue-400"
+        className="fixed text-white bottom-5 right-5 w-10 h-10 rounded-full dark:bg-blue-500 bg-blue-400"
         aria-label="up!"
         onClick={onClickScroll}
       >
