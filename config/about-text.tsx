eb2e21a6821e68bca36f5be2ed32cbf3ad6d3c9e
healthpackdev <yasinkadirkilic@gmail.com>
2021-06-17 15:20:40 +0300

    style: change about text

--- config/about-text.tsx
+++ config/about-text.tsx
@@ -2,7 +2,9 @@ import Link from 'next/link';
 
 const AboutText: React.FC = () => (
   <>
-    I&apos;m a Web Developer and 7th grade student at <b>Şair Nedim</b>. I currently work on this website and Golang -
+    I&apos;m a Web Developer and 7th grade student at <b>Şair Nedim</b>. I have about 1 years experience in Javascript.
+    I love <b>Next.js</b> and <b>React</b> also I know <b>Typescript</b>. I currently work on <b>this website</b> and{' '}
+    <b>Golang</b> -{' '}
     <Link href="/about">
       <a>Learn More About me</a>
     </Link>
