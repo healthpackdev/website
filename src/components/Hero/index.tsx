32baf55a6a5f43229dfc625b5c38830362fc0a8a
healthpackdev <yasinkadirkilic@gmail.com>
2021-06-19 14:51:09 +0300

    ui: use Section component in Hero, Write shortdescription manuel

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,9 +1,43 @@
-import HeroAbout from '@components/Hero/hero-about';
+import { Section } from '@components/section';
+import author from '@config/author-meta.json';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import type { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { motion } from 'framer-motion';
+import css from './Hero.module.css';
+import Link from 'next/link';
+
+const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
 
 const Hero: React.FC = () => (
-  <section id="hero" className="flex items-center lg:my-20 my-16">
-    <HeroAbout />
-  </section>
+  <Section header="Hi I'm Yasin Kadir" id="hero">
+    <p className="text-secondary">
+      I&apos;m a Web Developer and 7th grade student at Şair Nedim. I have about 1 years experience in Javascript. I
+      love <b>Next.js</b> and <b>React</b> with <b>Typescript</b>. I currently develop{' '}
+      <Link href="/">
+        <a>this website</a>
+      </Link>{' '}
+      and <b> trying to learn Golang</b> -{' '}
+      <Link href="/about">
+        <a>learn more about me</a>
+      </Link>
+    </p>
+
+    <div className="mt-5 grid lg:block grid-cols-2">
+      {author.socials.map((social, index) => (
+        <motion.a
+          className={css.social + ' no-link'}
+          whileHover={{ y: -2 }}
+          href={`/${social.icon[1]}`}
+          key={index.toString()}
+          //@ts-ignore
+          style={{ '--social-color': social.color }}
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={social.icon as IconProp} /> {capitalize(social.icon[1])}
+        </motion.a>
+      ))}
+    </div>
+  </Section>
 );
 
 export default Hero;
