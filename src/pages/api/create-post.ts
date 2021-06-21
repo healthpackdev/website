c32b7a8479680f4c77e50c2c1ec17c87303cbe1f
healthpackdev <yasinkadirkilic@gmail.com>
2021-06-21 22:15:46 +0300

    feat: add production create post

--- src/pages/api/create-post.ts
+++ src/pages/api/create-post.ts
@@ -1,7 +1,9 @@
 import type { NextApiHandler } from 'next';
-// import client from '@lib/octokit';
+import client from '@lib/octokit';
 import fs from 'fs';
 import path from 'path';
+import siteConfig from '@config/site-config.json';
+import author from '@config/author-meta.json';
 
 const createPostInDev = (body: Record<string, string>) => {
   fs.writeFileSync(
@@ -15,18 +17,31 @@ ${body.content}`
   );
 };
 
-// const createRepositoryInDev = (body: Record<string, string>) => {};
+const createPostInProd = async ({ slug, content }: Record<string, string>) => {
+  await client('PUT /repos/{owner}/{repo}/contents/{path}', {
+    owner: author.github,
+    content,
+    message: `create ${slug}.mdx`,
+    path: `content/blog/${slug}.mdx`,
+    repo: siteConfig.publicRepoName,
+    branch: siteConfig.branch,
+  });
+};
 
 const convertToString = (content: string, encoding: BufferEncoding) => Buffer.from(content, encoding).toString();
 
-const createPost: NextApiHandler = ({ headers, body, method }, res) => {
+const createPost: NextApiHandler = async ({ headers, body, method }, res) => {
   if (method === 'POST') {
     const reqToken = convertToString(headers.authorization.split(' ')[1], 'base64');
 
     if (reqToken !== process.env.ADMIN_TOKEN) {
       res.status(401).send('Unauthorized');
     } else {
-      if (process.env.NODE_ENV !== 'production') createPostInDev(body);
+      if (process.env.NODE_ENV !== 'production') {
+        createPostInDev(body);
+      } else {
+        await createPostInProd(body);
+      }
 
       res.status(200).json(body);
     }
