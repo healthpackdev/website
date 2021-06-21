cf69385cd5c28fdf6b2baadcf0ed2012ef0177c7
healthpackdev <yasinkadirkilic@gmail.com>
2021-06-21 11:59:22 +0300

    chore: don't use netlify-cms

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,45 +1,10 @@
-import dynamic from 'next/dynamic';
-import Head from 'next/head';
-import config from '@config/netlify-cms';
-import fonts from '@config/fonts.json';
-import { useEffect } from 'react';
-import type { NetlifyCmsApp } from 'netlify-cms-app';
+const Admin: Page = () => {
+  return <>Admin Page</>;
+};
 
-const NetlifyCMS = dynamic(
-  () =>
-    import('netlify-cms-app')
-      // @ts-ignore
-      .then((cms: typeof NetlifyCmsApp) => {
-        cms.init({ config });
-
-        // eslint-disable-next-line react/display-name
-        return () => <></>;
-      })
-      .then((fragment) => fragment),
-  {
-    ssr: false,
-  }
-);
-
-const headLinks: React.HTMLProps<HTMLLinkElement>[] = [
-  { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-  { rel: 'stylesheet', href: fonts.sans.url },
-];
-
-const Admin: React.FC = () => {
-  useEffect(() => {
-    document.body.classList.add('font-primary');
-  });
-  return (
-    <>
-      <Head>
-        {headLinks.map((data, index) => (
-          <link {...data} key={index.toString()} />
-        ))}
-      </Head>
-      <NetlifyCMS />
-    </>
-  );
+Admin.PageProps = {
+  title: 'Admin',
+  description: 'Public Admin page to create posts or edit posts',
 };
 
 export default Admin;
