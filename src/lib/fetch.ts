3b58f000755f35e24c322be099c2515fdc7ed08c
healthpackdev <yasinkadirkilic@gmail.com>
2021-07-01 15:37:32 +0300

    refactor: make data fetching cacheable

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -2,16 +2,16 @@ import useSWR from 'swr';
 
 type UseAPI = (endpoint: string, fetchOptions?: RequestInit) => { res: any; error: any };
 
-const apiFetcher = async (options: any[]) => {
-  const endpoint = options[0];
-  const fetchOptions = options[1];
-
-  const res = await fetch(`/api/${endpoint}`, fetchOptions);
-  return res.json();
+const apiFetcher = (endpoint, fetchOptions) => {
+  return new Promise((resolve) => {
+    resolve(fetch(`/api/${endpoint}`, fetchOptions).then((res) => res.json()));
+  });
 };
 
 export const useAPI: UseAPI = (endpoint, fetchOptions) => {
-  const { data, error } = useSWR([endpoint, fetchOptions], apiFetcher);
+  const { data, error } = useSWR([endpoint, fetchOptions], (endpoint, fetchOptions) =>
+    apiFetcher(endpoint, fetchOptions)
+  );
 
   return {
     res: data,
