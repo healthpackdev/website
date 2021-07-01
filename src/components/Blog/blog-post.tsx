f53907842ac93e50cf858947b092af694e5ac07c
healthpackdev <yasinkadirkilic@gmail.com>
2021-07-01 16:08:56 +0300

    refactor: remove unused var and comments

--- src/components/Blog/blog-post.tsx
+++ src/components/Blog/blog-post.tsx
@@ -2,23 +2,10 @@ import type { IBlogPostMatter } from '@lib/mdx';
 import css from './Blog.module.css';
 import dayjs from 'dayjs';
 import Link from 'next/link';
-import Image from 'next/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const BlogPost: React.FC<{ post: IBlogPostMatter }> = ({ post: { data, slug } }) => (
   <article className={css.article}>
-    {/* <div className="md:col-span-2 flex items-bottom">
-      <Image
-        src={require(`public/images/${data.image}`)}
-        objectFit="cover"
-        className="rounded-md"
-        width="200" // prefer 200x100 near images.
-        height="100"
-        objectPosition="center center"
-        alt={data.title}
-      />
-</div> */}
-
     <Link href={`/blog/${slug}`}>
       <a>
         <h2 className="text-blue-500 hover:underline text-3xl">{data.title}</h2>
