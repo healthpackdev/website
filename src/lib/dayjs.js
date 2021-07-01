956ed1db20bd1474d0af4ef61c7e6332f5ed5817
healthpackdev <yasinkadirkilic@gmail.com>
2021-07-01 16:07:57 +0300

    style: updateLocale tr, write '1' insteadof 'bir'

--- src/lib/dayjs.js
+++ src/lib/dayjs.js
@@ -3,4 +3,22 @@ import 'dayjs/locale/tr';
 
 dayjs.extend(require('dayjs/plugin/relativeTime'));
 dayjs.extend(require('dayjs/plugin/duration'));
+dayjs.extend(require('dayjs/plugin/updateLocale'));
 dayjs.locale('tr');
+dayjs.updateLocale('tr', {
+  relativeTime: {
+    future: '%s içinde',
+    past: '%s önce',
+    s: 'birkaç saniye',
+    m: '1 dakika',
+    mm: '%d dakika',
+    h: '1 saat',
+    hh: '%d saat',
+    d: '1 gün',
+    dd: '%d gün',
+    M: '1 ay',
+    MM: '%d ay',
+    y: '1 yıl',
+    yy: '%d yıl',
+  },
+});
