d9d94ed742702d2f071c26466c613485c3dc9e8e
healthpackdev <yasinkadirkilic@gmail.com>
2021-07-04 12:24:38 +0300

    ui: hidden hashtag in h3, h4, h5

--- src/components/mdx/Link.jsx
+++ src/components/mdx/Link.jsx
@@ -1,18 +1,28 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHashtag } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
 
-export default function a({ className, children, ...p }) {
+export default function a({ className, children, href, ...p }) {
   // if element has anchor class transform it hashtag
   if (className?.includes('anchor')) {
     return (
-      <a {...p}>
+      <a {...p} href={href} className={className}>
         <FontAwesomeIcon icon={faHashtag} className="mr-2" />
       </a>
     );
   }
+  if (href.startsWith('/')) {
+    return (
+      <Link href={href}>
+        <a {...p} className={className}>
+          {children}
+        </a>
+      </Link>
+    );
+  }
   // if doesn't spread props
   return (
-    <a {...p} className={className}>
+    <a {...p} className={className} href={href}>
       {children}
     </a>
   );
