dad1fcdd1eff6f5ea1c73bc015041c0269fa77bd
healthpackdev <yasinkadirkilic@gmail.com>
2021-07-04 12:24:58 +0300

    ui: add link to navbar header

--- src/layout/Navbar/index.tsx
+++ src/layout/Navbar/index.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { NavbarMobileToggle, NavbarMobile } from '@layout/Navbar/navbar-mobile';
 import NavbarLinks from '@layout/Navbar/navbar-links';
 import NavbarGithub from '@layout/Navbar/navbar-github';
+import Link from 'next/link';
 
 import ThemeToggle from '@components/theme-toggle';
 import pkg from 'package.json';
@@ -11,10 +12,14 @@ const Navbar: React.FC = () => {
 
   return (
     <header className="lg:mt-16 mt-5 max-w-content">
-      <div>
-        <h2 className="pl-3 mr-1 text-3xl inline-block font-semibold">Healthpack</h2>
-        <span className="font-comic select-none font-bold bg-blue-500 text-white px-1 rounded-md">v{pkg.version}</span>
-      </div>
+      <Link href="/">
+        <a>
+          <h2 className="pl-3 mr-1 text-3xl inline-block font-semibold">Healthpack</h2>
+          <span className="font-comic select-none font-bold bg-blue-500 text-white px-1 rounded-md">
+            v{pkg.version}
+          </span>
+        </a>
+      </Link>
       <div className="border-b py-2 flex justify-between">
         <div className="flex items-center">
           <NavbarMobileToggle onClick={() => setIsNavOpen(!isNavOpen)} isNavOpen={isNavOpen} />
