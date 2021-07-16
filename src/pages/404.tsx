7b3133c42f27de9d49ffca99978691f05de2069e
healthpackdev <yasinkadirkilic@gmail.com>
2021-07-16 21:31:53 +0300

    ui: customize 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,18 @@
+import { useRouter } from 'next/router';
+
 const Error: Page = () => {
-  return <>404</>;
+  const router = useRouter();
+
+  return (
+    <div className="p-4 flex justify-center items-center flex-col">
+      <h4 className="text-3xl mb-4">
+        <span className="text-red-500">404</span> - are your lose you way?
+      </h4>
+      <button className="btn" onClick={() => router.push('/')}>
+        Return Home.
+      </button>
+    </div>
+  );
 };
 
 Error.layoutProps = { title: '404', description: 'Not found page' };
