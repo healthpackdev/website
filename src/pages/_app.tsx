b1d346d7aa0a306aa385588ac6844e8720ef1e6f
healthpackdev <yasinkadirkilic@gmail.com>
2021-07-17 17:00:53 +0300

    refactor: use NextPage type for App page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,13 @@ import '@lib/font-awesome';
 import '@lib/dayjs';
 import '@theme/main.css';
 import Layout from '@layout/index';
+import type { NextPage } from 'next';
 
 if (process.env.NODE_ENV !== 'production') {
   import('preact/debug');
 }
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App: NextPage<AppProps> = ({ Component, pageProps }) => {
   let layoutProps = (Component as Page).layoutProps;
 
   if (typeof layoutProps === 'function') layoutProps = layoutProps(pageProps);
