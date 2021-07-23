b49a14518a2d82ce96914d9061960f3fb8686f18
healthpackdev <yasinkadirkilic@gmail.com>
2021-07-23 18:43:33 +0300

    build: don't use preact in dev mode

--- next.config.js
+++ next.config.js
@@ -1,22 +1,23 @@
 const author = require('./config/author-meta.json');
 const StatoscopeWebpackPlugin = require('@statoscope/webpack-plugin').default;
-const Prefresh = require('@prefresh/next');
 
 /**
  * @type import('next/dist/next-server/server/config-shared').NextConfig
  */
-const config = {
+module.exports = {
   webpack(config, { isServer, dev }) {
     if (isServer) {
       require('./src/lib/sitemap.js');
     }
 
     // replace preact with react
-    Object.assign(config.resolve.alias, {
-      react: 'preact/compat',
-      'react-dom/test-utils': 'preact/test-utils',
-      'react-dom': 'preact/compat',
-    });
+    if (!dev) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
 
     // analyze client and server the bundle in ANALYZE mode.
     if (process.env.ANALYZE === 'true') {
@@ -42,4 +43,3 @@ const config = {
     });
   },
 };
-module.exports = Prefresh(config);
