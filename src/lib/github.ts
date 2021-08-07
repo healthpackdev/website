ade3448e4477a8bc37c42375e9fa0d4644af008c
healthpackdev <yasinkadirkilic@gmail.com>
2021-08-07 21:43:13 +0300

    refactor: create reusable repo fetcher function

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -3,3 +3,10 @@ import { request as Request } from '@octokit/request';
 export const request = Request.defaults({
   headers: { authorization: `token ${process.env.GITHUB_ACCESS_TOKEN}` },
 });
+
+// get user repositories from github
+export const getUserRepositories = (username: string) => {
+  return request(`GET /users/{username}/repos`, {
+    username,
+  });
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,21 @@ import Techs from '@components/Techs';
 import Repos from '@components/Repos';
 import Contact from '@components/Contact';
 import { GetStaticProps } from 'next';
-import { request } from '@lib/github';
+import { getUserRepositories } from '@lib/github';
 
 import author from '@config/author-meta.json';
 
-interface IRepo {
-  readonly fork_count: number;
-  readonly lang: string;
-  readonly link: string;
-  readonly name: string;
-  readonly star: number;
-  readonly desc: string;
+interface Repo {
+  fork_count: number;
+  lang: string;
+  link: string;
+  name: string;
+  star: number;
+  desc: string;
 }
 
-export type ErrorOrRepo = IRepo[] | string;
 interface HomeProps {
-  repos: ErrorOrRepo;
+  repos: Repo[];
 }
 
 const Home: Page<HomeProps> = ({ repos }) => (
@@ -36,27 +35,19 @@ Home.layoutProps = {
 };
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
-  let repos: ErrorOrRepo;
-  await request(`GET /users/{username}/repos`, {
-    username: author.github,
-  })
-    .then(({ data }) => {
-      repos = data
-        .sort((a, b) => b.stargazers_count - a.stargazers_count)
-        .filter((project) => project.name !== author.github && !project.archived && !project.fork)
-        .slice(0, 12) // Get 12 max repo
-        .map((project) => ({
-          fork_count: project.forks_count,
-          name: project.name,
-          star: project.stargazers_count,
-          lang: project.language,
-          desc: project.description || 'No Description Provided',
-          link: project.html_url,
-        }));
-    })
-    .catch(() => {
-      repos = 'Rate Limit Error';
-    });
+  const { data } = await getUserRepositories(author.github);
+  const repos = data
+    .sort((a, b) => b.stargazers_count - a.stargazers_count)
+    .filter((project) => project.name !== author.github && !project.archived && !project.fork)
+    .slice(0, 12) // Get 12 max repo
+    .map((project) => ({
+      fork_count: project.forks_count,
+      name: project.name,
+      star: project.stargazers_count,
+      lang: project.language,
+      desc: project.description || 'No Description Provided',
+      link: project.html_url,
+    }));
 
   return {
     props: {
