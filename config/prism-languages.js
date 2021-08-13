3aae5885febff6585a88c4a945d9e0e366bcfed0
healthpackdev <yasinkadirkilic@gmail.com>
2021-08-13 17:27:40 +0300

    chore: remove prismjs

--- config/prism-languages.js
+++
@@ -1,6 +0,0 @@
-// require('prismjs/components/prism-tsx.min');
-require('prismjs/components/prism-jsx.min');
-require('prismjs/components/prism-typescript.min');
-require('prismjs/components/prism-bash.min');
-require('prismjs/components/prism-json.min');
-require('prismjs/components/prism-markdown.min');
