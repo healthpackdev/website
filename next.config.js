51382356fc786174dcb07a03d8c16b3178f454e2
Yasin <68531011+healthpackdev@users.noreply.github.com>
2022-08-04 19:55:59 +0000

    revert: drop preact

--- next.config.js
+++ next.config.js
@@ -2,23 +2,13 @@ const author = require('./config/author-meta.json');
 const StatoscopeWebpackPlugin = require('@statoscope/webpack-plugin').default;
 
 /**
- * @type import('next/dist/next-server/server/config-shared').NextConfig
+ * @type {import('next').NextConfig}
  */
 module.exports = {
   experimental: { esmExternals: 'loose' },
   webpack(config, { isServer, dev }) {
     if (isServer) {
       require('./src/lib/sitemap.js');
-    }
-
-    // replace preact with react
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
     }
 
     // analyze client and server the bundle in ANALYZE mode.
