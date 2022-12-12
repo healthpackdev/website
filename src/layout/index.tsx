2962a05680e96438b570df0f6306318dc0f1e5d5
Yasin <68531011+healthpackdev@users.noreply.github.com>
2022-12-12 18:35:53 +0000

    feat: snow effect in full page

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -2,12 +2,14 @@ import { Seo, SeoProps } from '@layout/Seo';
 import Navbar from '@layout/Navbar';
 import Footer from '@layout/Footer';
 import ScrollTop from '@layout/scroll-top';
+import Script from 'next/script';
 
 export type LayoutProps = SeoProps;
 
 const Layout: React.FC<LayoutProps> = ({ children, ...props }) => (
   <>
     <Seo {...props} />
+    <Script src="https://app.embed.im/snow.js" />
     <Navbar />
     <main className="max-w-content my-5">{children}</main>
     <ScrollTop />
